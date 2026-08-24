3e0fceacabbbe74b842959ccea00cca08591c256
shivam <yamashitacore721@gmail.com>
2026-08-24 23:09:36 +0530

    Add Longest-Substring-Without-Repeating-Characters (sliding-window)

---
+++ leetcode/sliding-window/3-Longest-Substring-Without-Repeating-Characters.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n=s.size();
+        unordered_set<char> m;
+        int left=0;
+        int maxi=0;
+        for(int i=0;i<n;i++){
+            while(m.count(s[i])){
+                m.erase(s[left]);
+                left++;
+            }
+            m.insert(s[i]);
+            maxi=max(maxi,i-left+1);
+        }
+        return maxi;
+    }
+};
