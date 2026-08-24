687249a079770cd9c1440fad4a2aac870b238663
shivam <yamashitacore721@gmail.com>
2026-08-24 22:36:54 +0530

    Add Halloumi Boxes (800)

--- codeforces/800/1903A - Halloumi Boxes.cpp
+++ codeforces/800/1903A - Halloumi Boxes.cpp
@@ -1,24 +1,27 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
  
-int main(){
-    int t;
-    cin>>t;
-    while(t--){
-    int n,k;
-    cin>>n>>k;
-    
-    vector<int> a(n);
-    for(int i=0; i<n; i++){
-        cin>>a[i];
-    }
-    vector<int> copy =a;
-    sort(copy.begin(), copy.end());
-    if(copy==a || k>1){
-        cout<<"YES"<<endl;
-    }
-    else{
-        cout<<"NO"<<endl;
-    }
-}
+int main() {
+  int t;
+  cin>>t;
+  while(t--){
+      int n,k;
+      cin>>n>>k;
+      vector<int> v(n);
+      for(int i=0;i<n;i++){
+          cin>>v[i];
+      }
+      vector<int> a=v;
+      sort(v.begin(),v.end());
+      if(v==a){
+          cout<<"YES"<<endl;
+      }
+      else if(k>1){
+          cout<<"YES"<<endl;
+      }
+      else{
+          cout<<"NO"<<endl;
+      }
+  }
+  return 0;
 }
