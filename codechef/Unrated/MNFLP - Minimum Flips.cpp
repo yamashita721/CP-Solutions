dde694b942b5d9c9a4159ebd61644ba8dd2242b9
shivam <yamashitacore721@gmail.com>
2026-08-24 19:24:34 +0530

    Add Minimum Flips (Unrated)

---
+++ codechef/Unrated/MNFLP - Minimum Flips.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    vector<int> v(n);
+	    int sum=0;
+	    for(int i=0;i<n;i++){
+	        cin>>v[i];
+	        sum+=v[i];
+	    }
+	    if(n%2!=0){
+	        cout<<-1<<endl;
+	    }
+	    else{
+	        cout<<abs(sum)/2<<endl;
+	    }
+	    
+	    
+	}
+
+}
