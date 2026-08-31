a7d06efa5547db4fe42bf1407db3a99992c26ab4
shivam <yamashitacore721@gmail.com>
2026-08-31 13:55:25 +0530

    Add Rotate-Array (two-pointers)

--- leetcode/two-pointers/189-Rotate-Array.cpp
+++ leetcode/two-pointers/189-Rotate-Array.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        int n=nums.size();//-1,-100,3,99
+        int n=nums.size();
         k%=n;
         reverse(nums.begin(),nums.end()-k);
         reverse(nums.end()-k,nums.end());
-        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.end());   
     }
 };
