e15dc49a1e8fceb273028e0f7dca5ff8a3a2c0c5
shivam <yamashitacore721@gmail.com>
2026-08-31 13:38:21 +0530

    Add Remove-Duplicates-from-Sorted-Array (two-pointers)

--- leetcode/two-pointers/26-Remove-Duplicates-from-Sorted-Array.cpp
+++ leetcode/two-pointers/26-Remove-Duplicates-from-Sorted-Array.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        if(nums.size()==0) return 0;
         int k=1;
         for(int i=1;i<nums.size();i++){
             if(nums[i]!=nums[k-1]){
