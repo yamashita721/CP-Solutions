1ef9fe9ff317ab605c8bbd7b67f7dfd9ddb4d6fb
shivam <yamashitacore721@gmail.com>
2026-08-31 13:34:36 +0530

    Add Reverse-Integer (math)

---
+++ leetcode/math/7-Reverse-Integer.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int reverse(int x) {
+    
+        int num=0;
+        while(x!=0){
+            int last_digit=x%10;
+
+            if(num>(INT_MAX)/10 || (num==(INT_MAX)/10 && last_digit>7)){
+                return 0;
+            }
+
+            if(num<(INT_MIN)/10 || (num==(INT_MIN)/10 && last_digit<-8)){
+                return 0;
+            }
+            num=num*10+last_digit;
+            x/=10;
+        }
+        return num;
+    }
+};
