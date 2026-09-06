d869a4a42cb130f4e897783417074592cd09041e
shivam <yamashitacore721@gmail.com>
2026-09-06 16:40:14 +0530

    Add Top-K-Frequent-Elements (heap-priority-queue)

--- leetcode/heap-priority-queue/347-Top-K-Frequent-Elements.cpp
+++ leetcode/heap-priority-queue/347-Top-K-Frequent-Elements.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        map<int,int> m;
+        unordered_map<int,int> m;
         for(int i=0;i<nums.size();i++){
             m[nums[i]]++;
         }
