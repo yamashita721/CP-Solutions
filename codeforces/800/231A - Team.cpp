d1de2b7fb6ecfcd2a69d3bb1394bcb724f92a74f
shivam <yamashitacore721@gmail.com>
2026-09-07 18:13:18 +0530

    Add Team (800)

---
+++ codeforces/800/231A - Team.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int count=0;
+    while(n--){
+        int a,b,c;
+        cin>>a>>b>>c;
+        if((a==1 && b==1) || (a==1 && c==1) || (b==1 && c==1)){
+            count++;
+        }
+    }
+    cout<<count<<endl;
+}
