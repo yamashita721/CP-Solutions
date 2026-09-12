e57436510650bfac9832b05aff0a1db1a464e9af
shivam <yamashitacore721@gmail.com>
2026-09-13 00:15:36 +0530

    Add Calculating Function (800)

---
+++ codeforces/800/486A - Calculating Function.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+int main(){
+    ll n;
+    cin>>n;
+    if(n%2==0){
+        cout<<n/2<<endl;
+    }
+    else{
+        cout<<-(n+1)/2<<endl;
+    }
+}
