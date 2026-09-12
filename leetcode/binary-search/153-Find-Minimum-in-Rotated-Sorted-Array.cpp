a9b665c0f98089247119dd863af6faaf6f9cdfe5
shivam <yamashitacore721@gmail.com>
2026-09-13 03:54:23 +0530

    Add Find-Minimum-in-Rotated-Sorted-Array (binary-search)

--- leetcode/binary-search/153-Find-Minimum-in-Rotated-Sorted-Array.cpp
+++ leetcode/binary-search/153-Find-Minimum-in-Rotated-Sorted-Array.cpp
@@ -1,7 +1,17 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        return nums[0];
+        int left=0;
+        int right=nums.size()-1;
+        while(left<right){
+            int mid=left+(right-left)/2;
+            if(nums[mid]<=nums[right]){
+                right=mid;
+            }
+            else{
+                left=mid+1;
+            }
+        }
+        return nums[left];
     }
 };
