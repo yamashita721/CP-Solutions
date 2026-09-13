69a0e90a6b4fb1e87c8209006fb95afb9a8072f3
shivam <yamashitacore721@gmail.com>
2026-09-13 22:32:59 +0530

    Add Expression (1000)

---
+++ codeforces/1000/479A - Expression.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  int a,b,c;
+  cin>>a>>b>>c;
+  vector<int> v={a,b,c};
+  sort(v.begin(),v.end());
+  cout<<max({a*b*c, (a*b)+c, a*(b+c), (a+b)*c, a+b+c})<<endl;
+  return 0;
+}
